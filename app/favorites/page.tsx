814fd6e08692eef36ca84615e9506564776688f1
Jay Kapade <jazzdestructor@gmail.com>
2023-05-23 22:09:52 +0530

    Added Favorite page

---
+++ app/favorites/page.tsx
@@ -0,0 +1,32 @@
+import EmptyState from "@/app/components/EmptyState";
+import ClientOnly from "@/app/components/ClientOnly";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getFavoriteListings from "@/app/actions/getFavoriteListings";
+import ListingsClient from "../components/TripsClient";
+import FavoritesClient from "./FavoritesClient";
+
+const FavoriteListingsPage = async () => {
+  const listings = await getFavoriteListings();
+  console.log("🚀 ~ listings:", listings);
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No favorites found"
+          subtitle="Looks like you have no favorite listings."
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <FavoritesClient listings={listings} currentUser={currentUser} />
+    </ClientOnly>
+  );
+};
+
+export default FavoriteListingsPage;
