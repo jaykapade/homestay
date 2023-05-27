bafdb258af19d1bb5b755a6b4df3b50dd324b25b
Jay Kapade <jazzdestructor@gmail.com>
2023-05-27 14:36:40 +0530

    disabled to remove eslint warning

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -71,6 +71,7 @@ const RentModal = () => {
       dynamic(() => import("../Map"), {
         ssr: false,
       }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [location]
   );
 
